76621580f3249c8ecc6d3b6d486be1e1d6f505af
Tharun R <Tharuntechie@gmail.com>
2024-09-21 13:03:16 +0530

    Updated

---
+++ src/Java_Variables_Datatypes.java
@@ -0,0 +1,13 @@
+public class Java_Variables_Datatypes {
+
+    public static void main(String[] args) {
+        int ticketPrice;
+        ticketPrice = 100;
+        System.out.println(ticketPrice);
+        long pow = (long) Math.pow(2,64);
+        System.out.println(pow);
+        int len = (int) Math.log10(pow) + 1;
+        System.out.println(len);
+
+    }
+}
