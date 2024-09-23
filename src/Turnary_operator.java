018d02e7f258082c7e71c4c0a23282e903e8c559
Tharun R <Tharuntechie@gmail.com>
2024-09-24 01:14:36 +0530

    Turnary operations added

---
+++ src/Turnary_operator.java
@@ -0,0 +1,10 @@
+public class Turnary_operator {
+    public static void main(String[] args) {
+    int a = 10;
+    boolean res = a > 18 ? true:false;
+        System.out.println(res);
+    }
+
+
+
+}
