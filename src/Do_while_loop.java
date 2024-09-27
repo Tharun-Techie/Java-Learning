90cf2a7c3bd90fc8919ee3b1519a14f196f498cb
Tharun R <Tharuntechie@gmail.com>
2024-09-27 12:51:19 +0530

    Loops are added

---
+++ src/Do_while_loop.java
@@ -0,0 +1,13 @@
+public class Do_while_loop {
+
+    public static void main(String[] args) {
+        int count = 0;
+
+        do{
+            System.out.println("Hello Java");
+            count++;
+        }while (count<5);
+
+        System.out.println("Count value is :"+ count);
+    }
+}
