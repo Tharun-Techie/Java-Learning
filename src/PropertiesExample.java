003e81db2fd26d73aae0eeaa25ad0d5a25fe9093
Tharun R <Tharuntechie@gmail.com>
2024-09-28 21:24:29 +0530

    2 files added

---
+++ src/PropertiesExample.java
@@ -0,0 +1,20 @@
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class PropertiesExample {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        
+        // Load properties from a file
+        try (FileInputStream input = new FileInputStream("config.properties")) {
+            properties.load(input);
+            String username = properties.getProperty("username");
+            String password = properties.getProperty("password");
+            System.out.println("Username: " + username);
+            System.out.println("Password: " + password);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
