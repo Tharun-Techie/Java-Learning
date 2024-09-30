90146213893d4a0682859bf3ba60fbeea1b76f78
Tharun R <Tharuntechie@gmail.com>
2024-09-30 15:41:13 +0530

    day 95

--- src/Java_continue.java
+++ src/Java_continue.java
@@ -1,2 +1,17 @@
 public class Java_continue {
+
+    public static void main(String[] args) {
+        int a = 100;
+        int b =a;
+        while (a==b){
+            System.out.println(a);
+                a++;
+            System.out.println(b);
+                continue;
+        }
+        a++;
+        while (a!=a){
+            break;
+        }
+    }
 }
