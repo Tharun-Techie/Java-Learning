69be5111aff71bbb4242b3a50710390c690ffe7b
Tharun R <Tharuntechie@gmail.com>
2024-10-08 15:05:40 +0530

    3d array added

---
+++ src/Jagged_array.java
@@ -0,0 +1,7 @@
+public class Jagged_array {
+    public static void main(String[] args) {
+        // Jahagged Array ,a 2d array with multiple colums with different sizes,, for eack rows
+
+
+    }
+}
