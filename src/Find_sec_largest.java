790c043e5e76f7052b5c2b7809f2c8d80aa8c36c
Tharun R <Tharuntechie@gmail.com>
2024-10-22 11:17:06 +0530

    File updated

---
+++ src/Find_sec_largest.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+
+public class Find_sec_largest {
+
+    public static void main(String[] args) {
+        int[] arr = {12,34,32,25,13,31,2,3,6};
+            for(int a:arr){
+                a = (int) (Math.random() * 10);
+
+        System.out.println(a);
+            }
+    }
+}
+
