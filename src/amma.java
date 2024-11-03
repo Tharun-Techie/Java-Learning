2dc5b8a6e856f8d382df08d3b4d5c97aa2973577
Tharun R <Tharuntechie@gmail.com>
2024-11-03 12:17:37 +0530

    Hope started

---
+++ src/amma.java
@@ -0,0 +1,7 @@
+public class amma {
+    public static void main(String[] args) {
+        System.out.println("Hello Sunitha!!!!!");
+        System.out.println("Yenna pandrinngeee!!!");
+        System.out.println(5255+5244);
+    }
+}
