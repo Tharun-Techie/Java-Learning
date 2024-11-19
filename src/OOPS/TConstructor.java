291561c65515c0500121a576cfc0dc3fe4b431b1
Tharun R <Tharuntechie@gmail.com>
2024-11-19 12:45:23 +0530

    19/11/204

---
+++ src/OOPS/TConstructor.java
@@ -0,0 +1,13 @@
+package OOPS;
+
+class Human{
+    Human(){
+        System.out.println("Hello!!, Constructor here");
+    }
+}
+
+public class TConstructor {
+    public static void main(String[] args) {
+        Human obj = new Human();
+    }
+}
