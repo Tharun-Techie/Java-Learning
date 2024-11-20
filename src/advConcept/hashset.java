749709a94b12adfb7a9714a42d0dc4dd2efe265d
Tharun R <Tharuntechie@gmail.com>
2024-11-20 22:47:14 +0530

    learnt some random topic

--- src/advConcept/hashset.java
+++ src/advConcept/hashset.java
@@ -3,13 +3,29 @@ package advConcept;
 import java.util.HashSet;
 import java.util.Set;
 
+
 public class hashset {
-    public static void main(String[] args) {
-        Set<String> val = new HashSet<>();
-        val.add("Tharun");
-        if (val.contains("Tharun")){
-            System.out.println("yes bruh....");
+
+    static boolean solution(int[] nums, int k){
+         Set<Integer> val = new HashSet<>();
+
+        for(int arr: nums){
+            if ( val.contains(arr)== false){
+                val.add(arr);
+            }
+            else if (val.contains(arr) == true){
+                return true;
+            }
+            else {
+                return false;
+            }
         }
-        System.out.println(val);
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,1};
+        int k = 3;
+        System.out.println(solution(nums,k));
     }
 }
