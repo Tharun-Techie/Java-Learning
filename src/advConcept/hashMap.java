bf2c73b73782978c2f09eecd4e0ffff58512dbe0
Tharun R <Tharuntechie@gmail.com>
2024-12-13 01:38:41 +0530

    Hope is the only thing

---
+++ src/advConcept/hashMap.java
@@ -0,0 +1,12 @@
+package advConcept;
+import java.util.HashMap;
+import java.util.Map;
+
+public class hashMap {
+    public static void main(String[] args){
+        Map<String, Integer> nameMap = new HashMap<>();
+        nameMap.put("Tharun R",22);
+        nameMap.put("Rajan",28);
+
+    }
+}
--- src/advConcept/hashset.java
+++ src/advConcept/hashset.java
@@ -29,3 +29,4 @@ public class hashset {
         System.out.println(solution(nums,k));
     }
 }
+
