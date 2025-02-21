f675348c97a59131125c0ebffb2a161b3b444344
Tharun R <Tharuntechie@gmail.com>
2025-02-21 12:51:30 +0530

    Notes Updated

--- src/Dump/Test1.java
+++ src/Dump/Test1.java
@@ -1,4 +1,31 @@
 package Dump;
+class Demo{
+    public static void demo(){
+        System.out.println("I am from Demo 1");
+    }
+}
+
+class  Demo2{
+    public  void demo2(){
+        System.out.println("I am From Demo 2");
+    }
+}
+
 
 public class Test1 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+
+        Demo.demo();
+
+        Demo2 object = new Demo2();
+        object.demo2();
+
+    }
+
 }
+
+
+
